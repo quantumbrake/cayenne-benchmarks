44434bdebb94b79fec296b4a2dc5818f640839e3
Dileep Kishore <k.dileep1994@gmail.com>
2019-05-18 15:23:53 -0400

    Add: Bash script to run all benchmarks

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "pyssa direct"
+python pyssa_test/simulation.py "direct"
+echo "pyssa tau_leaping"
+python pyssa_test/simulation.py "tau_leaping"
+echo "pyssa tau_adaptive"
+python pyssa_test/simulation.py "tau_adaptive"
+echo "tellurium"
+python -W ignore tellurim_test/simulation.py
+echo "biomsim.jl"
+julia --procs=1 biosimjl_test/model1.jl
+echo "GillespieSSA.R"
+Rscript GillespieSSA_test/simulation.R
