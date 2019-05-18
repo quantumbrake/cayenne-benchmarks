80613cf0bec79e3dd8cca021623801ef3dba1014
Dileep Kishore <k.dileep1994@gmail.com>
2019-05-18 15:21:15 -0400

    Add: Profiling to GillespieSSA

--- GillespieSSA_test/simulation.R
+++ GillespieSSA_test/simulation.R
@@ -14,22 +14,30 @@ simName <- "Decaying-Dimerizing Reaction Set"
 
 # Run simulations 
 
+"
 # Direct method
 set.seed(1)
-out <- ssa(x0,a,nu,parms,tf,method="D",simName,verbose=TRUE,consoleInterval=1) 
+out <- ssa(x0,a,nu,parms,tf,method='D',simName,verbose=TRUE,consoleInterval=1) 
 ssa.plot(out,show.title=TRUE,show.legend=FALSE)
 
 # Explict tau-leap method
 set.seed(1)
-out <- ssa(x0,a,nu,parms,tf,method="ETL",simName,tau=0.003,verbose=TRUE,consoleInterval=1) 
+out <- ssa(x0,a,nu,parms,tf,method='ETL',simName,tau=0.003,verbose=TRUE,consoleInterval=1) 
 ssa.plot(out,show.title=FALSE,show.legend=FALSE) 
 
 # Binomial tau-leap method
 set.seed(1)
-out <- ssa(x0,a,nu,parms,tf,method="BTL",simName,verbose=TRUE,consoleInterval=1) 
+out <- ssa(x0,a,nu,parms,tf,method='BTL',simName,verbose=TRUE,consoleInterval=1) 
 ssa.plot(out,show.title=FALSE,show.legend=FALSE) 
+"
 
 # Optimized tau-leap method
-set.seed(1)
-out <- ssa(x0,a,nu,parms,tf,method="OTL",simName,verbose=TRUE,consoleInterval=1) 
+# set.seed(1)
+start_time = Sys.time()
+# OTL
+for (i in 1:10) {
+    out <- ssa(x0,a,nu,parms,tf,method='D',simName,verbose=FALSE,consoleInterval=1) 
+}
+end_time = Sys.time()
+print(paste("Time elapsed in seconds:", end_time - start_time))
 ssa.plot(out,show.title=FALSE,show.legend=FALSE) 
