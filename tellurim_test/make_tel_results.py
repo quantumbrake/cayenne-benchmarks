4c397d2f4d59b27832a63abbe6c5338605d1a938
Dileep Kishore <k.dileep1994@gmail.com>
2020-02-01 17:34:55 -0500

    Add: Simulation scripts for the tellurium library

--- tellurim_test/make_tel_results.py
+++ tellurim_test/make_tel_results.py
@@ -1,48 +1,33 @@
 #!/usr/bin/env python3
 
-import time
+import sys
+
+import numpy as np
+import tellurium as te
+
+from models import models, nreps
 
 
-def te_simulation():
-model = """
-r1: S1 => ; k1*S1;
-r2: S1 + S1 => S2; k2*S1*(S1-1);
-r3: S2 => S1 + S1; k3*S2;
-r4: S2 => S3; k4*S2;
+def te_simulation(id_="00001"):
 
-k1 = 1;
-k2 = 0.002;
-k3 = 0.5;
-k4 = 0.04;
+    model = models[id_]
 
-S1 = 4150;
-S2 = 39565;
-S3 = 3445;
-"""
+    te_model = te.loada(model)
+    te_model.integrator = "gillespie"
+    te_model.integrator.seed = 1234
+    te_model.integrator.variable_step_size = False
 
-te_model = te.loada(model)
-te_model.integrator = "gillespie"
-# te_model.integrator.seed = 1234
-te_model.integrator.variable_step_size = False
-
-    nrep = 50
-    points = 1_000_000
+    nrep = nreps[id_]
+    points = 50
 
     sim_list = []
     for i in range(nrep):
         te_model.reset()
-        sim = te_model.simulate(0, 10, steps=points)
+        sim = te_model.simulate(0, 50, steps=points)
         sim_list.append(sim)
-        np.savetxt("temp2.txt", np.array(sim), delimiter=",")
-        te_model.plot(sim, alpha=0.7, show=False)
-
+        np.savetxt(f"results/{id_}/tellurium/{i + 1}.csv", np.array(sim), delimiter=",")
 
 
 if __name__ == "__main__":
-    start_time = time.time()
-    import tellurium as te
-
-    te_simulation()
-    end_time = time.time()
-    print(f"Elapsed time in seconds: {end_time - start_time }")
-    plot_results()
+    MODEL_ID = sys.argv[1]
+    te_simulation(MODEL_ID)
