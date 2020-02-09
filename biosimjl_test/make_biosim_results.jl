134f385bb768bab3ecded47a4b2ffe7970df1926
Dileep Kishore <k.dileep1994@gmail.com>
2020-02-08 19:18:53 -0500

    Add: More models for BioSimulator.jl tests

--- biosimjl_test/make_biosim_results.jl
+++ biosimjl_test/make_biosim_results.jl
@@ -1,50 +1,12 @@
 using BioSimulator
 using DelimitedFiles
 
-function model_00001()
-    # initialize
-    model = Network("Model 00001")
-    # species definitions
-    model <= Species("S1", 100)
-    # rate definitions
-    k1 = 0.1
-    k2 = 0.11
-    # reaction definitions
-    model <= Reaction("birth", k1, "S1 --> S1 + S1")
-    model <= Reaction("death", k2, "S1 --> 0")
-    return model
-end
-
-function model_00003()
-    # initialize
-    model = Network("Model 00003")
-    # species definitions
-    model <= Species("S1", 100)
-    # rate definitions
-    k1 = 1.0
-    k2 = 1.1
-    # reaction definitions
-    model <= Reaction("birth", k1, "S1 --> S1 + S1")
-    model <= Reaction("death", k2, "S1 --> 0")
-    return model
-end
-
-
-function get_model(model_name)
-    if model_name == "00001"
-        model = model_00001()
-    elseif model_name == "00003"
-        model = model_00003()
-    else
-        error("Unsupported model_name")
-    end
-    return model
-end
+include("models.jl")
 
 function run_model(model, algorithm)
     # simulation parameters
-    nrep = 10000
-    time_final = 50.0
+    nrep = 200
+    time_final = 51.0
     epochs = 50
     # simulate
     if algorithm == "direct"
@@ -56,15 +18,20 @@ function run_model(model, algorithm)
     else
         error("Unsupported algorithm")
     end
-    result = simulate(model, algo_func, time=time_final, epochs=epochs, trials=nrep, output_type=Val(:fixed))
+    result = simulate(model, algo_func, Val(:full), time=time_final, epochs=epochs, trials=nrep)
     return result
 end
 
 function writemodel(result, dir_name)
     mkpath(dir_name)
     for i = 1:length(result.simulation_data)
+        temp_array = []
+        for j = 1:length(result.simulation_data[i].tdata)
+            row = hcat(result.simulation_data[i].tdata[j], result.simulation_data[i].xdata[j][:])
+            push!(temp_array, hcat(row))
+        end
         open(string(dir_name, i, ".csv"), "w") do io
-            writedlm(io, hcat(result.simulation_data[i].tdata[:], result.simulation_data[i].xdata[:]), ",")
+            writedlm(io, temp_array, ",")
         end
     end
 end
---
+++ biosimjl_test/models.jl
@@ -0,0 +1,186 @@
+using BioSimulator
+
+####################
+# System 1
+####################
+
+function model_00001()
+    # initialize
+    model = Network("Model 00001")
+    # species definitions
+    model <= Species("S1", 100)
+    # rate definitions
+    k1 = 0.1
+    k2 = 0.11
+    # reaction definitions
+    model <= Reaction("birth", k1, "S1 --> S1 + S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00003()
+    model = Network("Model 00003")
+    model <= Species("S1", 100)
+    k1 = 1.0
+    k2 = 1.1
+    model <= Reaction("birth", k1, "S1 --> S1 + S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00004()
+    model = Network("Model 00004")
+    model <= Species("S1", 10)
+    k1 = 0.1
+    k2 = 0.11
+    model <= Reaction("birth", k1, "S1 --> S1 + S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00005()
+    model = Network("Model 00005")
+    model <= Species("S1", 10000)
+    k1 = 0.1
+    k2 = 0.11
+    model <= Reaction("birth", k1, "S1 --> S1 + S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00011()
+    model = Network("Model 00011")
+    model <= Species("S1", 100)
+    k1 = 0.1 / 2
+    k2 = 0.11 / 2
+    model <= Reaction("birth", k1, "S1 --> S1 + S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00011()
+    model = Network("Model 00011")
+    model <= Species("S1", 100)
+    k1 = 0.1 / 2
+    k2 = 0.11 / 2
+    model <= Reaction("birth", k1, "S1 --> S1 + S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+####################
+# System 2
+####################
+
+function model_00020()
+    model = Network("Model 00020")
+    model <= Species("S1", 0)
+    k1 = 1.0
+    k2 = 0.1
+    model <= Reaction("immigration", k1, "0 --> S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00021()
+    model = Network("Model 00021")
+    model <= Species("S1", 0)
+    k1 = 10.0
+    k2 = 0.1
+    model <= Reaction("immigration", k1, "0 --> S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00022()
+    model = Network("Model 00022")
+    model <= Species("S1", 0)
+    k1 = 5.0
+    k2 = 0.1
+    model <= Reaction("immigration", k1, "0 --> S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00023()
+    model = Network("Model 00023")
+    model <= Species("S1", 0)
+    k1 = 1000
+    k2 = 0.1
+    model <= Reaction("immigration", k1, "0 --> S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+####################
+# System 3
+####################
+
+function model_00030()
+    model = Network("Model 00030")
+    model <= Species("S1", 100)
+    model <= Species("S2", 0)
+    k1 = 0.001 / 2
+    k2 = 0.01
+    model <= Reaction("dimerization", k1, "S1 + S1 --> S2")
+    model <= Reaction("monomerization", k2, "S2 --> S1 + S1")
+    return model
+end
+
+function model_00031()
+    model = Network("Model 00031")
+    model <= Species("S1", 1000)
+    model <= Species("S2", 0)
+    k1 = 0.0002 / 2
+    k2 = 0.004
+    model <= Reaction("dimerization", k1, "S1 + S1 --> S2")
+    model <= Reaction("monomerization", k2, "S2 --> S1 + S1")
+    return model
+end
+
+####################
+# System 4
+####################
+
+function model_00037()
+    model = Network("Model 00037")
+    model <= Species("S1", 0)
+    k1 = 1.0
+    k2 = 0.2
+    model <= Reaction("immigration", k1, "0 --> 5 * S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00038()
+    model = Network("Model 00038")
+    model <= Species("S1", 0)
+    k1 = 1.0
+    k2 = 0.4
+    model <= Reaction("immigration", k1, "0 --> 10 * S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+function model_00039()
+    model = Network("Model 00039")
+    model <= Species("S1", 0)
+    k1 = 1.0
+    k2 = 4.0
+    model <= Reaction("immigration", k1, "0 --> 100 * S1")
+    model <= Reaction("death", k2, "S1 --> 0")
+    return model
+end
+
+
+
+function get_model(model_name)
+    fn_name = string("model_", model_name)
+    try
+        fn = getfield(Main, Symbol(fn_name))
+        model = fn()
+        return model
+    catch UndefVarError
+        error("Unsupported model name")
+    end
+end
