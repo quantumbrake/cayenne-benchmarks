2be4a07b3cbcdf2eac77eb80e8b0d546230667ca
Dileep Kishore <k.dileep1994@gmail.com>
2020-02-15 17:04:51 -0500

    Add: All models for tellurium library testing

--- tellurim_test/make_tel_results.py
+++ tellurim_test/make_tel_results.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
 
+import pathlib
+import os
 import sys
 
 import numpy as np
 import tellurium as te
 
-from models import models, nreps
+from models import get_model
+
+N_REPS = 10_000
 
 
-def te_simulation(id_="00001"):
-
-    model = models[id_]
-
+def run_model(id_):
+    model = get_model(id_)
     te_model = te.loada(model)
     te_model.integrator = "gillespie"
     te_model.integrator.seed = 1234
-    te_model.integrator.variable_step_size = False
-
-    nrep = nreps[id_]
-    points = 50
-
-    sim_list = []
-    for i in range(nrep):
+    te_model.integrator.variable_step_size = True
+    results = []
+    for i in range(N_REPS):
         te_model.reset()
-        sim = te_model.simulate(0, 50, steps=points)
-        sim_list.append(sim)
-        np.savetxt(f"results/{id_}/tellurium/{i + 1}.csv", np.array(sim), delimiter=",")
+        sim = te_model.simulate(0, 50)
+        results.append(sim)
+    return results
+
+def write_model(results, dir_path):
+    os.makedirs(dir_path, exist_ok=True)
+    for i in range(N_REPS):
+        sim = np.array(results[i])
+        file_name = f"{dir_path}/{i + 1}.csv"
+        np.savetxt(file_name, sim, delimiter=",")
 
 
 if __name__ == "__main__":
     MODEL_ID = sys.argv[1]
-    te_simulation(MODEL_ID)
+    DIR_PATH = pathlib.Path(f"./results/{MODEL_ID}/tellurim/")
+    results = run_model(MODEL_ID)
+    write_model(results, DIR_PATH)
