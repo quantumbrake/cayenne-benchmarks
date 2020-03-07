1a56223c4d560cb3a2c00386d9979d33e84c00bb
sriki18 <srikiranc@gmail.com>
2020-03-07 14:20:59 -0800

    Change: Big updates to `test_accuracy_modular`
    - Make it a function, add docstring
    - Create a summary plot for 1 species

--- accuracy/test_accuracy_modular.py
+++ accuracy/test_accuracy_modular.py
@@ -1,29 +1,53 @@
 import sys
-from helpers import calculate_zy, read_results, process_r
-from helpers import read_results_2sp, calculate_zy_2sp, process_r_2sp
+from .helpers import calculate_zy, read_results, process_r, make_plot
+from .helpers import read_results_2sp, calculate_zy_2sp, process_r_2sp
 import numpy as np
 
 
-id_ = sys.argv[1]
-library = sys.argv[2]
-algo = sys.argv[3]
+def test_accuracy(id_:str, library:str, algo:str, nrep:int):
+    """Test the accuracy for a given model, library, algorithm and number
+    of reps.
 
-two_species_models = ["00030", "00031"]
+    Parameters
+    ----------
+    id_
+        Model id.
+    library
+        Name of the library.
+    algo
+        Name of the algorithm for that library.
+    nrep
+        Number of repetitions to run.
 
-if id_ not in two_species_models:
-    time_list, mu_list, std_list = read_results(id_)
-    res = process_r(id_, library=library, algo=algo)
-    Z, Y = calculate_zy(res, time_list, mu_list, std_list)
-else:
-    print("Using 2 species")
-    time_list, mu_list, std_list = read_results_2sp(id_)
-    res = process_r_2sp(id_, library=library, algo=algo)
-    Z, Y = calculate_zy_2sp(res, time_list, mu_list, std_list)
-# print(res.get_state(1.0))
-assert (-3 < Z).all()
-try:
-    assert (Z < 3).all()
-except AssertionError:
-    print(np.where(Z < 3))
-assert (-5 < Y).all()
-assert (Y < 5).all()
+    Returns
+    -------
+    failed_list : List[int]
+        Number of failures of each type (Z<-3, Z>3, Y<-5 and Y>5).
+    """
+
+    two_species_models = ["00030", "00031"]
+
+    plt_name = f"plots/{library}_{algo}_{id_}_{nrep}.pdf"
+    if id_ not in two_species_models:
+        time_list, mu_list, std_list = read_results(id_)
+        res = process_r(id_, library=library, algo=algo, n_reps=nrep)
+        Z, Y, mu_obs_list, std_obs_list = calculate_zy(
+            res, time_list, mu_list, std_list
+        )
+        make_plot(
+            time_list, mu_list, std_list, mu_obs_list, std_obs_list, Z, Y, plt_name
+        )
+    else:
+        print("Using 2 species")
+        time_list, mu_list, std_list = read_results_2sp(id_)
+        res = process_r_2sp(id_, library=library, algo=algo, n_reps=nrep)
+        Z, Y = calculate_zy_2sp(res, time_list, mu_list, std_list)
+
+    # TODO: Does this work for two species?
+    failed_list = [None, None, None, None]
+    failed_list[0] = np.sum(Z < -3)
+    failed_list[1] = np.sum(Z > 3)
+    failed_list[2] = np.sum(Y < -5)
+    failed_list[3] = np.sum(Y > 5)
+
+    return failed_list
