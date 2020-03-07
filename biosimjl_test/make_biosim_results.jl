1dbdf8e1a78ef9697b7f455fdab769d8b6639568
Dileep Kishore <k.dileep1994@gmail.com>
2020-03-07 17:08:55 -0500

    Fix: Write model function for 2 species

--- biosimjl_test/make_biosim_results.jl
+++ biosimjl_test/make_biosim_results.jl
@@ -3,9 +3,8 @@ using DelimitedFiles
 
 include("models.jl")
 
-function run_model(model, algorithm)
+function run_model(model, algorithm, nreps)
     # simulation parameters
-    nrep = 200
     time_final = 51.0
     epochs = 50
     # simulate
@@ -18,16 +17,22 @@ function run_model(model, algorithm)
     else
         error("Unsupported algorithm")
     end
-    result = simulate(model, algo_func, Val(:full), time=time_final, epochs=epochs, trials=nrep)
+    result = simulate(model, algo_func, Val(:full), time=time_final, epochs=epochs, trials=nreps)
     return result
 end
 
-function writemodel(result, dir_name)
+function writemodel(result, dir_name, nspecies)
     mkpath(dir_name)
     for i = 1:length(result.simulation_data)
         temp_array = []
         for j = 1:length(result.simulation_data[i].tdata)
-            row = hcat(result.simulation_data[i].tdata[j], result.simulation_data[i].xdata[j][:])
+            t = result.simulation_data[i].tdata[j]
+            x = result.simulation_data[i].xdata[j]
+            if nspecies == 2
+                row = hcat(t, x[1], x[2])
+            else
+                row = hcat(t, x)
+            end
             push!(temp_array, hcat(row))
         end
         open(string(dir_name, i, ".csv"), "w") do io
@@ -39,7 +44,13 @@ end
 # Actual execution
 model_name = ARGS[1]
 algorithm = ARGS[2]
+nreps = parse(Int64, ARGS[3])
 model = get_model(model_name)
-result = run_model(model, algorithm)
-dir_name = string("./results/", model_name, "/BioSim_", algorithm, "/")
-writemodel(result, dir_name)
+if model_name == "00030" || model_name == "00031"
+    nspecies = 2
+else
+    nspecies = 1
+end
+result = run_model(model, algorithm, nreps)
+dir_name = string("./results/", model_name, "/BioSimulator_", algorithm, "/")
+writemodel(result, dir_name, nspecies)
