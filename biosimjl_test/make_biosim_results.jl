f3bec76dd6894550f47ffe1f24aedda4796b2ecc
Dileep Kishore <k.dileep1994@gmail.com>
2020-04-11 17:21:12 -0400

    Update: Update BioSimulator to v0.9.3

--- biosimjl_test/make_biosim_results.jl
+++ biosimjl_test/make_biosim_results.jl
@@ -1,43 +1,38 @@
 using BioSimulator
-using DelimitedFiles
+using CSV
+using DataFrames
+using BioSimulator: tablefy
 
 include("models.jl")
 
-function run_model(model, algorithm, nreps)
+function run_model(model, algorithm, nreps, interpolation)
     # simulation parameters
     time_final = 51.0
-    epochs = 50
     # simulate
     if algorithm == "direct"
         algo_func = Direct()
     elseif algorithm == "tau_leaping"
-        algo_func = TauLeaping()
+        # NOTE: This is an adaptive tau-leaping algorithm
+        algo_func = TauLeapingDGLP2003()
     elseif algorithm == "tau_adaptive"
-        algo_func = StepAnticipation()
+        algo_func = HybridSAL()
     else
         error("Unsupported algorithm")
     end
-    result = simulate(model, algo_func, Val(:full), time=time_final, epochs=epochs, trials=nreps)
-    return result
+    if interpolation == "True"
+        results = [simulate(model, algo_func, tfinal=time_final, save_points=0:1:time_final) for _ in 1:nreps]
+    else
+        results = [simulate(model, algo_func, tfinal=time_final, save_points=nothing) for _ in 1:nreps]
+    end
+    return results
 end
 
-function writemodel(result, dir_name, nspecies)
+function write_results(results, dir_name, nspecies)
+    print(results)
     mkpath(dir_name)
-    for i = 1:length(result.simulation_data)
-        temp_array = []
-        for j = 1:length(result.simulation_data[i].tdata)
-            t = result.simulation_data[i].tdata[j]
-            x = result.simulation_data[i].xdata[j]
-            if nspecies == 2
-                row = hcat(t, x[1], x[2])
-            else
-                row = hcat(t, x)
-            end
-            push!(temp_array, hcat(row))
-        end
-        open(string(dir_name, i, ".csv"), "w") do io
-            writedlm(io, temp_array, ",")
-        end
+    for i = 1:length(results)
+        file_name = string(dir_name, i, ".csv")
+        CSV.write(file_name, DataFrame(tablefy(results[i])), delim=',', writeheader=false)
     end
 end
 
@@ -45,12 +40,14 @@ end
 model_name = ARGS[1]
 algorithm = ARGS[2]
 nreps = parse(Int64, ARGS[3])
+interpolation = ARGS[4]
 model = get_model(model_name)
 if model_name == "00030" || model_name == "00031"
     nspecies = 2
 else
     nspecies = 1
 end
-result = run_model(model, algorithm, nreps)
-dir_name = string("./results/", model_name, "/BioSimulator_", algorithm, "/")
-writemodel(result, dir_name, nspecies)
+results = run_model(model, algorithm, nreps, interpolation)
+folder_name = interpolation == "True" ? "/BioSimulatorIntp_" : "/BioSimulator_"
+dir_name = string("./results/", model_name, folder_name, algorithm, "/")
+write_results(results, dir_name, nspecies)
