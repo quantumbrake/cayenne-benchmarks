e7867d4cc8e11802071f813aead69c07049f32ac
Dileep Kishore <k.dileep1994@gmail.com>
2020-04-19 14:54:40 -0400

    Add: Run benchmarks now only runs uncompleted benchmarks

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import pathlib
 from subprocess import Popen, PIPE, TimeoutExpired
 import sys
 
@@ -28,9 +29,11 @@ def get_benchmark_cmd(lib: str, model: str, algo: str, nrep: int) -> str:
     """
     sim_cmd = get_cmd(lib, model, algo, nrep)
     # TODO: Add this flag to all the scripts
-    fname = f"benchmarks/{lib}_{algo}_{model}_{nrep}.json"
-    benchmark_cmd = f"hyperfine --runs 3 --export-json {fname} '{sim_cmd} False'"
-    return benchmark_cmd
+    fname = f"benchmarks/{lib}-{algo}-{model}-{nrep}.json"
+    benchmark_cmd = (
+        f"hyperfine --runs 7 --export-json {fname} --show-output '{sim_cmd} False'"
+    )
+    return fname, benchmark_cmd
 
 
 def benchmark_simulation(
@@ -55,10 +58,16 @@ def benchmark_simulation(
     print(
         f"Running library: {lib}, algorithm: {algo}, model: {model} with nrep = {nrep}"
     )
-    cmd = get_benchmark_cmd(lib, model, algo, nrep)
+    fname, cmd = get_benchmark_cmd(lib, model, algo, nrep)
+    fpath = pathlib.Path(fname)
+    if fpath.exists() and fpath.is_file():
+        print(f"Benchmarks already exist for {fpath.stem}")
+        return None
     proc = Popen(cmd, shell=True, stderr=PIPE, stdout=PIPE)
     try:
         stdout, stderr = proc.communicate(timeout=timeout)
+        # print(stdout)
+        # print(stderr)
     except TimeoutExpired:
         print(f"{cmd} timeout")
         proc.kill()
